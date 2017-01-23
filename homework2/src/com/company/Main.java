36a7d6bf5c056563b4304095cbc34453abcad901
Benjamin Horn <hornbd96@gmail.com>
2017-01-23 10:32:39 -0500

    Hw2
    Hw2 started

---
+++ homework2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.io.*;
+
+public class Main extends PolynomialsA{
+
+    public static void main(String[] args) {
+	// write your code here
+    PolynomialsA object = new PolynomialsA();
+    object.irreducibles();
+
+
+    }
+}
