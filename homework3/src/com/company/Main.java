527d37c50e2dce8dbe3d01dcd8db725a8a2a4e66
Benjamin Horn <hornbd96@gmail.com>
2017-01-30 23:47:06 -0500

    AES HW

---
+++ homework3/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main extends AESA {
+
+    public static void main(String[] args) {
+        AESA object = new AESA();
+        String[] args2= {"stallingskey.rtf", "world"};
+
+        object.main(args2);
+
+    }
+}
