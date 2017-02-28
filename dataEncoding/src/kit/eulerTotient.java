1530f2b686ec87ec28366768dfe435b376568e63
Benjamin Horn <hornbd96@gmail.com>
2017-02-27 21:10:23 -0500

    Euler totient added + unit tests

---
+++ dataEncoding/src/kit/eulerTotient.java
@@ -0,0 +1,42 @@
+package kit;
+
+/**
+ * Created by benjamin.horn on 2/27/17.
+ */
+public class eulerTotient {
+    private int in_value;
+    private int totient_number;
+
+    public eulerTotient() {
+    }
+
+    public int getValue() {
+        return in_value;
+    }
+
+    public void setValue(int in_value) {
+        this.in_value = in_value;
+    }
+
+    public int euclidGCD(int a , int b){ // recursive get gcd of both
+        if(b == 0){
+            return a;
+        }
+        return euclidGCD(b, a%b);
+    }
+
+    public int getTotient_number() {
+        if(in_value == 0){
+            return 0;
+        }
+        totient_number = 1;
+        for(int i = 2; i < in_value; i++){
+            if(euclidGCD(i,in_value) == 1){
+                totient_number ++;
+            }
+        }
+
+        return totient_number;
+    }
+
+}
---
+++ dataEncoding/test/kit/eulerTotientTest.java
@@ -0,0 +1,45 @@
+package kit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by benjamin.horn on 2/27/17.
+ */
+public class eulerTotientTest {
+    @Test
+    public void tot1() throws Exception {
+        eulerTotient object = new eulerTotient();
+        object.setValue(12);
+        assertEquals(object.getTotient_number(), 4);
+    }
+
+    @Test
+    public void tot2() throws Exception {
+        eulerTotient object = new eulerTotient();
+        object.setValue(100);
+        assertEquals(object.getTotient_number(), 40);
+    }
+
+    @Test
+    public void tot3() throws Exception {
+        eulerTotient object = new eulerTotient();
+        object.setValue(1);
+        assertEquals(object.getTotient_number(), 1);
+    }
+
+    @Test
+    public void tot4() throws Exception {
+        eulerTotient object = new eulerTotient();
+        object.setValue(0);
+        assertEquals(object.getTotient_number(), 0);
+    }
+
+    @Test
+    public void tot5() throws Exception {
+        eulerTotient object = new eulerTotient();
+        object.setValue(1000);
+        assertEquals(object.getTotient_number(), 400);
+    }
+}
